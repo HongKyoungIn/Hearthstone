da39290e0cd34aa9fca6584b14d09f67dfc7007f
HongKyoungIn <ghdruddls999@naver.com>
2024-02-12 20:16:00 +0900

    스크립트 모든 줄마다 주석 달기 - v1

--- Hearthstone/Assets/Scripts/CardManager.cs
+++ Hearthstone/Assets/Scripts/CardManager.cs
@@ -7,14 +7,19 @@ public class CardManager : MonoBehaviour {
     public static CardManager Inst { get; private set; } // 매니저는 하나만 존재하기 때문에 싱글톤으로 선언.
     void Awake() => Inst = this;
 
+    /*
+    [SerializeField]
+    - 인스펙터 창에 노출시켜 접근 가능하지만 다른 외부 스크립트에선 수정을 못하게 한다.
+      즉, private 변수이지만 inspector 창에서 접근 가능한다.
+    */
     [SerializeField] ItemSO itemSO;
-    [SerializeField] GameObject cardPrefab; 
-    [SerializeField] List<Card> myCards; // 새로 추가
-    [SerializeField] List<Card> otherCards; // 새로 추가
+    [SerializeField] GameObject cardPrefab; // GameObject : Unity의 기본 객체 타입입니다. 모든 게임 오브젝트는 GameObject 클래스의 인스턴스입니다. GameObject 타입의 cardPrefab 변수를 선언.
+    [SerializeField] List<Card> myCards; // Card 타입의 리스트인 myCards를 선언.(내 카드 리스트)
+    [SerializeField] List<Card> otherCards;// Card 타입의 리스트인 otherCards를 선언.(상대 카드 리스트)
 
-    List<Item> itemBuffer;
+    List<Item> itemBuffer; // Item 타입의 리스트인 itemBuffer를 선언. 카드 덱을 담고있는 버퍼를 의미한다.
 
-    public Item PopItem() {
+    public Item PopItem() { // Item을 꺼내는 메서드. 즉, 다음 카드를 뽑는 메서드
         if (itemBuffer.Count == 0) { // 만약 카드를 다 뽑아 버퍼가 가지고 있는 카드의 갯수가 0개가 되면
             SetupItemBuffer(); // 다시 새로 버퍼에 100장의 카드를 셋팅
         }
@@ -24,25 +29,27 @@ public class CardManager : MonoBehaviour {
         return item; // 카드를 뽑아낸다.
     }
 
-    void SetupItemBuffer() {
-        itemBuffer = new List<Item>();
-        for(int i = 0; i < itemSO.items.Length; i++) { // item 배열에 담겨있는 10개의 카드
+    void SetupItemBuffer() { // Buffer에 아이템을 채우는 메서드
+        itemBuffer = new List<Item>(); // Item 타입의 객체를 담을 수 있는 새로운 리스트를 생성하고, 그것을 itemBuffer 변수에 할당하는 것을 의미합니다. 즉, itemBuffer라는 이름의 Item 객체 리스트를 생성하는 것을 의미합니다. 
+        for (int i = 0; i < itemSO.items.Length; i++) { // item 배열에 담겨있는 10개의 카드
             Item item = itemSO.items[i]; // 10개의 카드를 가져온다.
             for(int j = 0; j < item.percent; j++) { // 각각의 카드만큼의 퍼센트 만큼 반복시킨다
                 itemBuffer.Add(item); // 총 100장의 카드가 들어가며 각가의 카드 갯수는 각 카드의 퍼센트만큼 들어간다.
             }
         }
 
-        for(int i = 0; i < itemBuffer.Count; i++) { // 순서대로 들어가있는 카드를 랜덤하게 섞어준다.
-            int rand = Random.Range(i, itemBuffer.Count);
-            Item temp = itemBuffer[i];
-            itemBuffer[i] = itemBuffer[rand];
-            itemBuffer[rand] = temp;
+        // 버퍼에 순서대로 들어가있는 카드를 랜덤하게 섞어주는 반복문.
+        for (int i = 0; i < itemBuffer.Count; i++) { // 0부터 버퍼의 크기만큼 반복
+            int rand = Random.Range(i, itemBuffer.Count); // rand 변수에 0 ~ 99까지 랜덤한 수를 뽑는다.
+            Item temp = itemBuffer[i]; // 임시 Item 타입의 변수 temp에 현재 버퍼의 i번째 정보를 저장한다.
+            itemBuffer[i] = itemBuffer[rand]; // 현재 버퍼의 i번째 정보를 랜덤하게 뽑은 버퍼의 rand번째 정보로 바꾼다.
+            itemBuffer[rand] = temp; // 버퍼의 rand번째 정보는 i번째 정보로 바꾼다.
+            // 즉, 위 과정을 모두 수행하면 현재 i번째 정보와 랜덤하게 뽑은 rand번째 정보가 서로 바뀐다.
         }
     }
 
     void Start() {
-        SetupItemBuffer();
+        SetupItemBuffer(); // 버퍼에 100장의 카드를 확률에 근거하여 랜덤한 순서로 설정한다.
     }
 
     void Update() {
--- Hearthstone/Assets/Scripts/SO/ItemSO.cs
+++ Hearthstone/Assets/Scripts/SO/ItemSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
+[System.Serializable] // 이 클래스의 인스턴스를 Unity에서 직렬화 할 수 있게 합니다.
 public class Item {
     public string name;
     public int attack;
@@ -12,6 +12,9 @@ public class Item {
 }
 
 [CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Object/ItemSO")]
+
+// ItemSO 클래스는 Unity의 ScriptableObject를 상속받습니다. 
+// ScriptableObject : 게임에 특정 데이터를 저장하거나 공유하는 데 사용하는 클래스로, 일반적으로 게임 설정, AI 상태, 인벤토리 아이템 등을 저장하는 데 사용됩니다.
 public class ItemSO : ScriptableObject {
-    public Item[] items;
+    public Item[] items; // items는 Item 타입의 객체들을 담는 배열입니다. Item 클래스의 인스턴스들, 즉 Item 객체들이 items 배열에 저장됩니다.
 }
